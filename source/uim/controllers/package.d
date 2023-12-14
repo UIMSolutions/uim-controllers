d99304eb7cd47ead2c37edfcb6596046cc18c51a
UIMSolutions <ui.manufaktur@gmail.com>
2023-12-14 11:59:49 +0100

    Updates

---
+++ source/uim/controllers/package.d
@@ -0,0 +1,14 @@
+module uim.controllers;
+
+public { // required uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
+public { // controllers packages
+  import uim.controllers.classes;
+  import uim.controllers.interfaces;
+  import uim.controllers.helpers;
+  import uim.controllers.mixins;
+  import uim.controllers.tests;
+}
